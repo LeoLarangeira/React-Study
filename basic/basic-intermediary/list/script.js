68bac16f001d4f5e59b38b744aacface45049d67
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-06-06 11:08:16 -0300

    scripts with principal functions

--- basic/basic-intermediary/list/script.js
+++ basic/basic-intermediary/list/script.js
@@ -26,3 +26,62 @@ lista.filter((element, i, listRef)=>{
         
     }
 })
+
+
+//map 
+
+class Pessoa{
+    constructor(nome, idade){
+        this.nome = nome 
+        this.idade = idade
+    }
+}
+
+const pessoaLista = [new Pessoa('Leo', 22) , new Pessoa('Leo2', 22), new Pessoa('Leo3', 22), new Pessoa('Leo4', 22), new Pessoa('Leo5', 22)]
+
+console.log(pessoaLista)
+
+const listaNomes = []
+
+
+for (let i = 0; i < pessoaLista.length; i++) {
+    const element = array[i];
+    listaNomes.push(pessoaLista[i]['nome'])
+
+}
+console.log(listaNomes)
+
+//ou podemos fazer o seguinte 
+
+const listaMap = pessoaLista.map((element) =>{
+        //console.log(element.nome)
+        //return element.nome
+        return `
+            <li>
+               Nome: ${element.nome}
+               Idade: ${element.idade}
+            </li>
+        `
+})
+
+console.log(listaMap)
+
+
+//reduce 
+
+const somaDeTodosOsNumeros = lista.reduce((previous, current) =>{
+    console.log(previous, current)
+    return previous + current
+}, 0)
+
+console.log(somaDeTodosOsNumeros)
+
+
+//filter
+const listaNome = [{nome: 'Leo'}, {nome: 'foo'}, {nome: 'baar'}, {nome: 'Leozin'}]
+
+console.log(listaNome.filter((e)=> e.nome.startsWith('f')))
+
+const elementosHTML = listaNome.filter((e) => e.nome.startsWith('Leo')).map(e => `<li> ${e.nome} </li>`).join('')
+
+console.log(elementosHTML)
